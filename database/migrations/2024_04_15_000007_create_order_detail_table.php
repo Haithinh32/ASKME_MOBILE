0450587d82159b4b9039b55966fb443d704b120f
Haithinh32 <119341079+Haithinh32@users.noreply.github.com>
2024-04-15 10:47:49 +0700

    database

---
+++ database/migrations/2024_04_15_000007_create_order_detail_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('orderId')->unsigned();
+            $table->bigInteger('productId')->unsigned();
+            $table->foreign('orderId')->references('id')->on('order');
+            $table->foreign('productId')->references('id')->on('product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_detail');
+    }
+};
