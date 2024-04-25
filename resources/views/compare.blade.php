70e7e7ea26d928b9f966571ff41fcb9233c94572
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-04-25 20:24:20 +0700

    init compare

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -1,17 +1,15 @@
 @extends('components.mainlayout')
 @section('maincontent')
-<table>
-    <thead>
-        <tr>
-            <th>Phone Name</th>
-            <th>Brand</th>
-            <th>RAM</th>
-            <th>Disk</th>
-            <th>Chip</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
+
+
+<div>
+    <h1>This is compare page</h1>
+    @isset($products)
+        @foreach ($products as $product)
+
+        @endforeach
+    @endisset
+</div>
+
+
 @endsection
