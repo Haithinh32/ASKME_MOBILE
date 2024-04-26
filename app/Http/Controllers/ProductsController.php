b50453cb807de9351ebf5c07da8badb2140ef97b
Haithinh32 <119341079+Haithinh32@users.noreply.github.com>
2024-04-26 14:19:31 +0700

    fix log out log in homepage

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Products;
-
 use function Pest\Laravel\get;
 
 class ProductsController extends PageController
@@ -14,6 +13,7 @@ class ProductsController extends PageController
                             ->join('brands','products.brandId','=','brands.id')
                             ->select('products.id','products.pname','brands.bname','products.price','products.image','products.description','products.updated_at')
                             ->orderBy('products.updated_at')
+<<<<<<< Updated upstream
                             ->limit(24)
                             ->get();
         if (request()->has('search'))
@@ -36,6 +36,60 @@ class ProductsController extends PageController
                     'listproducts' => $searched
                 ]
             );
+=======
+                            ->simplePaginate(24);
+                            if (request()->has('search'))
+                            {
+                                $products = DB::table('products')->get();
+                                // dd($search_products);
+                                $searched = [];
+                                $search_string = request()->get('search','');
+                                foreach($products as $product)
+                                {
+                                    if(stripos($product->pname,$search_string) !== false or stripos($product->description,$search_string) !== false)
+                                    {
+                                        array_push($searched,$product);
+                                    }
+                                }
+                                // ->where('pname', 'like', '%' . request()->get('search', '') . '%');
+                                return view(
+                                    'homepage',
+                                    [
+                                        'listproducts' => $searched
+                                    ]
+                                );
+                            }
+                            //check if request have 'brand=..'
+                            return view('homepage',['listproducts' => $listProducts]);
+    }
+    public function index2(){
+        $AdminlistProduct = DB::table('products')
+                            ->join('brands','products.brandId','=','brands.id')
+                            ->select('products.id','products.pname','brands.bname','products.price','products.image','products.description','products.updated_at')
+                            ->orderBy('products.id')
+                            ->simplePaginate(5);
+        return view('dashboard',['listproducts' => $AdminlistProduct]);
+    }
+
+    public function addnew(Request $request){
+        $product = new Products();
+        $product->pname = $request->pname;
+        $product->brandId = $request->brandId;
+        $product->description = $request->description;
+        $product->specId = $request->specId;
+        $product->price = $request->price;
+        $product->image = $request->image;
+        $product->save();
+        return redirect()->route('dashboard');
+    }
+
+    public function deleteproduct(Request $request)
+    {
+        if ($request->isMethod('get') && $request->has('id')) {
+            $id = $request->input('id');
+            Products::where('id', $id)->delete();
+            return redirect()->route('dashboard');
+>>>>>>> Stashed changes
         }
         //check if request have 'brand=..'
 
