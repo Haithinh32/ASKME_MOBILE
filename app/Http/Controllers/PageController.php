0302986ed6911d6f91f8f3cbf917e07dc03885a3
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-05-06 18:31:45 +0700

    done tmp compare

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -37,23 +37,42 @@ class PageController
 
     public function compare()
     {
-        return view("compare");
+        $products_diff_brand = DB::table('products')
+            ->select('products.*', 'specs.*','brands.*')
+            ->distinct('brandid')
+            ->limit(5)
+            ->join('brands', 'products.brandid', '=', 'brands.id')
+            ->join('specs', 'products.specid', '=', 'specs.id')
+            ->get();
+
+
+        $products_same_brand = DB::table('products')
+            ->select('products.*', 'specs.*','brands.*')
+            ->where('brandid', 1)
+            ->limit(2)
+            ->join('brands', 'products.brandid', '=', 'brands.id')
+            ->join('specs', 'products.specid', '=', 'specs.id')
+            ->get();
+        return view("compare", [
+            "products_db" => $products_diff_brand,
+            "products_sb" => $products_same_brand
+        ]);
     }
     public function brand(Request $request)
     {
         $brand = DB::table('brands')
-        ->where('id','=',$request->get('id',1))
-        ->select('brands.id','brands.blogo','brands.bname')
-        ->first();
+            ->where('id', '=', $request->get('id', 1))
+            ->select('brands.id', 'brands.blogo', 'brands.bname')
+            ->first();
         $products = DB::table('products')
-        ->join('brands','products.brandId','=','brands.id')
-        ->where('brands.bname','=',$brand->bname)
-        ->select('products.id','products.pname','brands.bname','products.price','products.image','products.description','products.updated_at')
-        ->orderBy('products.updated_at')
-        ->paginate(24);
+            ->join('brands', 'products.brandId', '=', 'brands.id')
+            ->where('brands.bname', '=', $brand->bname)
+            ->select('products.id', 'products.pname', 'brands.bname', 'products.price', 'products.image', 'products.description', 'products.updated_at')
+            ->orderBy('products.updated_at')
+            ->paginate(24);
 
         // dd($brand,$products,$request->get('id'));
-        return view("brand",[
+        return view("brand", [
             'products' => $products,
             'brand' => $brand
         ]);
