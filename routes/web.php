5cd6c492bbaf9f14873ae568cfb4f70bcefc1e15
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-05-07 17:11:04 +0700

    restore page controller

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductsController;
-
-Route::get('/compare',[PageController::class, 'compare'])->name('compare');
-Route::get('/brand',[PageController::class, 'brand'])->name('brand');
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/homepage', function () {
-        return view('homepage');
-    })->name('homepage');
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+
+Route::get('/compare',[PageController::class, 'compare'])->name('compare');
+Route::get('/brand',[PageController::class, 'brand'])->name('brand');
+
 Route::get('/', function () {
     return view('homepage');
 });
