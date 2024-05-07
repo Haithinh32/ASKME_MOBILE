e80ef39a5bc916bb85e53c17596ec07fbf094e83
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-05-07 17:12:42 +0700

    remove unecessary flex class

--- resources/views/addnew.blade.php
+++ resources/views/addnew.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grid grid-cols-3 flex">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grid grid-cols-3">
                     <div class="col-span-2">
                         <div class="p-6 text-black ">
                             <form action="{{ route('addnew') }}" method="POST" enctype="multipart/form-data">
